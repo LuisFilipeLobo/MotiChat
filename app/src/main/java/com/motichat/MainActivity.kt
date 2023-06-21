8efbff84e484f3170f76ed5df658dc8055bec4f4
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2023-06-21 07:13:11 -0300

    Adicionando navegacao entre as activities

--- app/src/main/java/com/motichat/MainActivity.kt
+++ app/src/main/java/com/motichat/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.motichat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.motichat.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // Esconder a action bar
+        supportActionBar?.hide()
+
+        binding.btnSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_save) {
+            handleBtnSave()
+        }
+    }
+
+    private fun handleBtnSave() {
+        // Expressão regular para garantir que o nome do usuário possua apenas letras
+        val regex = Regex("^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\$")
+        val username = binding.editWhatsYourName.text.toString()
+
+        if (username.matches(regex)) {
+            startActivity(Intent(this, UserActivity::class.java))
+        } else {
+            // Exibir mensagem de erro
+            Toast.makeText(
+                this,
+                R.string.msg_error_invalid_username,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/motichat/UserActivity.kt
+++ app/src/main/java/com/motichat/UserActivity.kt
@@ -1,11 +1,28 @@
 package com.motichat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.motichat.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+
+        // Esconder a action bar
+        supportActionBar?.hide()
+
+        setContentView(binding.root)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_new_phrase) {
+            TODO("TODO")
+        }
     }
 }
